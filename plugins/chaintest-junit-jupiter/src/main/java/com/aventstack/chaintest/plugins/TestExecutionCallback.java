b7d0b7f7cf4827fde371b97fd4341803909891dd
Anshoo Arora <anshooarora@gmail.com>
2024-10-10 16:40:57 -0400

    Use ChainTestApiClient with default config

--- plugins/chaintest-junit-jupiter/src/main/java/com/aventstack/chaintest/plugins/TestExecutionCallback.java
+++ plugins/chaintest-junit-jupiter/src/main/java/com/aventstack/chaintest/plugins/TestExecutionCallback.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-import java.net.URI;
 import java.net.http.HttpResponse;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -22,21 +21,33 @@ public class TestExecutionCallback
         implements BeforeAllCallback, AfterAllCallback, BeforeTestExecutionCallback, AfterTestExecutionCallback {
 
     private static final AtomicBoolean CALLBACK_INVOKED = new AtomicBoolean();
-    private static final int MAX_RETRIES = 3;
+    private static final int DEFAULT_MAX_RETRIES = 3;
     private static final ConcurrentHashMap<String, WrappedResponseAsync<Test>> _tests = new ConcurrentHashMap<>();
+    private static final String PROPERTY_PLUGINS_MAX_RETRIES = "chaintest.plugins.max-retries";
 
-    private static ChainTestApiClient _client = new ChainTestApiClient(URI.create("http://localhost/api/v1/"));
+    private static ChainTestApiClient _client;
     private static Build _build = null;
+    private static int _maxRetries = DEFAULT_MAX_RETRIES;
 
     @Override
     public void beforeAll(final ExtensionContext extensionContext) throws Exception {
-        if (CALLBACK_INVOKED.getAndSet(true) || null == _client) {
+        if (CALLBACK_INVOKED.getAndSet(true)) {
             return;
         }
+        _client = new ChainTestApiClient();
+        loadConfig();
+
         final Build build = new Build();
         final HttpResponse<Build> response = _client.send(build, Build.class);
         if (200 == response.statusCode()) {
             _build = response.body();
+        }
+    }
+
+    private void loadConfig() {
+        final String maxRetries = _client.getConfig().get(PROPERTY_PLUGINS_MAX_RETRIES);
+        if (null != maxRetries && !maxRetries.isBlank() && maxRetries.chars().allMatch(Character::isDigit)) {
+            _maxRetries = Integer.parseInt(maxRetries);
         }
     }
 
@@ -72,7 +83,7 @@ public class TestExecutionCallback
                 .map(WrappedResponseAsync::getEntity)
                 .anyMatch(x -> x.getResult().equals(Result.FAILED.getResult()));
 
-        final HttpRetryHandler retryHandler = new HttpRetryHandler(_client, MAX_RETRIES);
+        final HttpRetryHandler retryHandler = new HttpRetryHandler(_client, _maxRetries);
         retryHandler.retryWithAsync(_tests);
 
         if (!_tests.isEmpty()) {
