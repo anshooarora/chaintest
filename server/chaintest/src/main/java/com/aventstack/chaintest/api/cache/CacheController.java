63f292d31d43f3f23ef32cc244ce37f0c3895ade
Anshoo Arora <anshooarora@gmail.com>
2024-10-16 12:10:59 -0400

    Adds RequestMapping path for /caches

--- server/chaintest/src/main/java/com/aventstack/chaintest/api/cache/CacheController.java
+++ server/chaintest/src/main/java/com/aventstack/chaintest/api/cache/CacheController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/caches")
 public class CacheController {
 
     @Autowired
