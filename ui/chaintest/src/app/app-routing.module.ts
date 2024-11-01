4f2851255b456a991929507f980517d2b5ce52a3
Anshoo Arora <anshooarora@gmail.com>
2024-10-31 22:34:03 -0400

    app module & routing initial

--- ui/chaintest/src/app/app-routing.module.ts
+++ ui/chaintest/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuildListingComponent } from './pages/build/build-listing/build-listing.component';
+import { ProjectListingComponent } from './pages/project/project-listing/project-listing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProjectListingComponent },
+  { path: 'projects', component: ProjectListingComponent },
+  { path: 'projects/:id/builds', component: BuildListingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ui/chaintest/src/app/app.module.ts
+++ ui/chaintest/src/app/app.module.ts
@@ -2,19 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BuildListingComponent } from './pages/build/build-listing/build-listing.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProjectListingComponent } from './pages/project/project-listing/project-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuildListingComponent,
+    FooterComponent,
+    HeaderComponent,
+    ProjectListingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgApexchartsModule
   ],
   providers: [
     provideClientHydration(),
