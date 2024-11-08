dbba05096c37aff768e56c91e0bfeec5947a8fe4
Anshoo Arora <anshooarora@gmail.com>
2024-11-07 21:25:11 -0500

    Update app-routing.module.ts

--- ui/chaintest/src/app/app-routing.module.ts
+++ ui/chaintest/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BuildListingComponent } from './pages/build/build-listing/build-listing.component';
 import { ProjectListingComponent } from './pages/project/project-listing/project-listing.component';
+import { TestListingComponent } from './pages/test/test-listing/test-listing.component';
 
 const routes: Routes = [
   { path: '', component: ProjectListingComponent },
   { path: 'projects', component: ProjectListingComponent },
-  { path: 'projects/:id/builds', component: BuildListingComponent }
+  { path: 'projects/:projectId/builds', component: BuildListingComponent },
+  { path: 'projects/:projectId/builds/:buildId/tests', component: TestListingComponent }
 ];
 
 @NgModule({
