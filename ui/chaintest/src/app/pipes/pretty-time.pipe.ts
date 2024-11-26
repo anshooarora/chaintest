8648d3b8b1edd3368f7b8893fb165fdfc3a8f4cb
Anshoo Arora <anshooarora@gmail.com>
2024-11-25 22:40:07 -0500

    PrettyTime pipe

---
+++ ui/chaintest/src/app/pipes/pretty-time.pipe.ts
@@ -0,0 +1,28 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'prettyTime'
+})
+export class PrettyTimePipe implements PipeTransform {
+
+  transform(value: number, ...args: unknown[]): unknown {
+    if (1000 > value) {
+      return `${value}ms`;
+    }
+    
+    const millis = value % 1000;
+    const secs = Math.floor((millis / 1000) % 60);
+    if (60000 > value) {
+      return `${secs}s`;
+    }
+
+    const mins = Math.floor((millis / (60 * 1000)) % 60);
+    if (3600000 > value) {
+      return `${mins}m ${secs}s`;
+    }
+
+    const hours = Math.floor((millis / (3600 * 1000)) % 3600);
+    return `${hours}h ${mins}m ${secs}s`;
+  }
+
+}
