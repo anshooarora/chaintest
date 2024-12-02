e6157ebe75b62a150bc1924a419f025d8ce600f1
Anshoo Arora <anshooarora@gmail.com>
2024-12-02 12:52:30 -0500

    Transactional test-history

--- server/chaintest/src/main/java/com/aventstack/chaintest/api/test/history/TestHistoryService.java
+++ server/chaintest/src/main/java/com/aventstack/chaintest/api/test/history/TestHistoryService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TestHistoryService {
@@ -22,6 +24,7 @@ public class TestHistoryService {
         return forTest(test, pageable);
     }
 
+    @Transactional(isolation = Isolation.READ_UNCOMMITTED)
     public Page<Test> forTest(final Test test, final Pageable pageable) {
         final TestHistorySpec spec = new TestHistorySpec(test);
         return repository.findAll(spec, pageable);
