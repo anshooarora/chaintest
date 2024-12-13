970d8ab6e6cc1ccb02901489b16f7840a2024872
Anshoo Arora <anshooarora@gmail.com>
2024-12-13 14:32:13 -0500

    BuildComponent: Annotated charts

--- ui/chaintest/src/app/pages/build/build/build.component.ts
+++ ui/chaintest/src/app/pages/build/build/build.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
-import { ChartData, ChartOptions, LegendItem } from 'chart.js';
+import { Chart, ChartData, ChartDataset, ChartOptions, LegendItem } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
+import AnnotationPlugin from "chartjs-plugin-annotation";
+
 import { BuildService } from '../../../services/build.service';
 import { ErrorHandlerService } from '../../../services/error-handler.service';
 import { Build } from '../../../model/build.model';
@@ -18,7 +20,7 @@ import { Test } from '../../../model/test.model';
 export class BuildComponent implements OnInit, OnDestroy {
 
   @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
-  
+    
   private _destroy$: Subject<any> = new Subject<any>();
 
   buildId: number;
@@ -32,7 +34,9 @@ export class BuildComponent implements OnInit, OnDestroy {
   constructor(private route: ActivatedRoute,
     private _buildService: BuildService,
     private _testService: TestService,
-    private _errorService: ErrorHandlerService) { }
+    private _errorService: ErrorHandlerService) { 
+      Chart.register(AnnotationPlugin); 
+    }
 
   ngOnInit(): void {
     let id = this.route.snapshot.paramMap.get('buildId') || '0';
@@ -53,54 +57,70 @@ export class BuildComponent implements OnInit, OnDestroy {
     this.displayCharts = !this.displayCharts;
   }
 
-  depth0Data: ChartData<any> = {
-    labels: ['Passed', 'Failed', 'Skipped'],
-    datasets: [
-      {
-        data: [],
-        backgroundColor: ['rgb(89, 199, 125)', 'rgb(240, 105, 132)', 'rgb(233, 210, 113)']
-      }
-    ]
+  dataset: ChartDataset<any> = {
+    backgroundColor: ['rgb(89, 199, 125)', 'rgb(240, 105, 132)', 'rgb(233, 210, 113)'],
+    borderColor: 'transparent'
   };
 
-  depth1Data: ChartData<any> = {
+  depth0: ChartData<any> = {
     labels: ['Passed', 'Failed', 'Skipped'],
-    datasets: [
-      {
-        data: [],
-        backgroundColor: ['rgb(89, 199, 125)', 'rgb(240, 105, 132)', 'rgb(233, 210, 113)']
-      }
-    ]
+    datasets: [{
+      data: [],
+      ...this.dataset
+    }]
   };
 
-  depth2Data: ChartData<any> = {
+  depth1: ChartData<any> = {
     labels: ['Passed', 'Failed', 'Skipped'],
-    datasets: [
-      {
-        data: [],
-        backgroundColor: ['rgb(89, 199, 125)', 'rgb(240, 105, 132)', 'rgb(233, 210, 113)']
-      }
-    ]
+    datasets: [{
+      data: [],
+      ...this.dataset
+    }]
   };
 
-  options: ChartOptions<any> = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'right',
-        labels: {
-          boxWidth: 15,
-          filter: (item: LegendItem, chartData: ChartData<any>) => {
-            const label = item.text.toUpperCase();
-            let index = 2;
-            label == 'PASSED' && (index = 0);
-            label == 'FAILED' && (index = 1);
-            return chartData.datasets[0].data[index] != 0;
+  depth2: ChartData<any> = {
+    labels: ['Passed', 'Failed', 'Skipped'],
+    datasets: [{
+      data: [],
+      ...this.dataset
+    }]
+  };
+
+  getOptions(depth: number): ChartOptions<any> {
+    const runStats = this.build.runStats.filter(x => x.depth == depth);
+    const label = Math.floor(runStats[0].passed / runStats[0].total * 100);
+    const options: ChartOptions<any> = {
+      responsive: true,
+      cutout: '70%',
+      plugins: {
+        legend: {
+          position: 'right',
+          labels: {
+            boxWidth: 15,
+            filter: (item: LegendItem, chartData: ChartData<any>) => {
+              const label = item.text.toUpperCase();
+              let index = 2;
+              label == 'PASSED' && (index = 0);
+              label == 'FAILED' && (index = 1);
+              return chartData.datasets[0].data[index] != 0;
+            }
           }
+        },
+        annotation: {
+          annotations: [{
+            borderWidth: 0,
+            label: {
+              content: label + '%',
+              display: true,
+              backgroundColor: 'transparent',
+              color: 'inherit'
+            }
+          }]
         }
       }
-    }
-  };
+    };
+    return options;
+  }
 
   findBuild(): void {
     this._buildService.find(this.buildId)
@@ -120,15 +140,15 @@ export class BuildComponent implements OnInit, OnDestroy {
   private computeMetrics(build: Build) {
     const depth0 = build.runStats.filter(x => x.depth == 0);
     if (depth0.length) {
-      this.depth0Data.datasets[0].data.push(depth0[0].passed, depth0[0].failed, depth0[0].skipped);
+      this.depth0.datasets[0].data.push(depth0[0].passed, depth0[0].failed, depth0[0].skipped);
     }
     const depth1 = build.runStats.filter(x => x.depth == 1);
     if (depth1.length) {
-      this.depth1Data.datasets[0].data.push(depth1[0].passed, depth1[0].failed, depth1[0].skipped);
+      this.depth1.datasets[0].data.push(depth1[0].passed, depth1[0].failed, depth1[0].skipped);
     }
     const depth2 = build.runStats.filter(x => x.depth == 2);
     if (depth2.length) {
-      this.depth2Data.datasets[0].data.push(depth2[0].passed, depth2[0].failed, depth2[0].skipped);
+      this.depth2.datasets[0].data.push(depth2[0].passed, depth2[0].failed, depth2[0].skipped);
     }
     this.chart?.update();
   }
