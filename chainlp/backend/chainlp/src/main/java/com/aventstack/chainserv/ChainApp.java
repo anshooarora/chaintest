55c892290fc95e9da3472ae9f08a4b1e5ca468f3
Anshoo Arora <anshooarora@gmail.com>
2024-12-14 16:20:34 -0500

    chainlp backend initial

---
+++ chainlp/backend/chainlp/src/main/java/com/aventstack/chainserv/ChainApp.java
@@ -0,0 +1,13 @@
+package com.aventstack.chainserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChainApp.class, args);
+    }
+
+}
