8007724b859b5672b31d66f164413752f616b74a
Anshoo Arora <anshooarora@gmail.com>
2024-12-15 17:22:07 -0500

    removes server/chaintest

--- server/chaintest/src/main/java/com/aventstack/chaintest/api/tagstats/TagStatsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aventstack.chaintest.api.tagstats;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TagStatsRepository extends CrudRepository<TagStats, Long> {
-}
