25680164275b011c847aeed181d8ce8bae4d251f
Anshoo Arora <anshooarora@gmail.com>
2025-01-06 15:25:22 -0500

    Remove sample tests from plugin

--- plugins/chaintest-junit5/src/test/java/com/aventstack/chaintest/plugins/RootTest.java
+++
@@ -1,7 +0,0 @@
-package com.aventstack.chaintest.plugins;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-
-@ExtendWith(ChainTestExecutionCallback.class)
-public class RootTest {
-}
--- plugins/chaintest-testng/src/test/java/plugins/AdditionTests.java
+++ plugins/chaintest-testng/src/test/java/plugins/AdditionTests.java
@@ -6,39 +6,14 @@ import static org.testng.Assert.assertEquals;
 
 public class AdditionTests extends RootTest {
 
-    @Test
+    @Test(groups = {"sum"})
     public void sum() {
         assertEquals(10, Math.addExact(6, 4));
     }
 
-    @Test(groups = {"pos"})
+    @Test(groups = {"sum"})
     public void sumLargeNumber() {
         assertEquals(1_000_006, Math.addExact(6, 1_000_000));
-    }
-
-    @Test(groups = {"pos"})
-    public void sumLargeNumbers() {
-        assertEquals(10_000_000, Math.addExact(9_000_000, 1_000_000));
-    }
-
-    @Test(groups = {"neg"})
-    public void sumNegative() {
-        assertEquals(-10, Math.addExact(-6, -4));
-    }
-
-    @Test(groups = {"neg"})
-    public void sumNegativeLargeNumbers() {
-        assertEquals(-10_000_000, Math.addExact(-9_000_000, -1_000_000));
-    }
-
-    @Test(groups = {"neg"})
-    public void sumNegativeLargeNumber() {
-        assertEquals(-1_000_001, Math.addExact(-1, -1_000_000));
-    }
-
-    @Test(groups = {"pos", "neg"})
-    public void sumOfNegativeAndPositive() {
-        assertEquals(10, Math.addExact(-6, 16));
     }
 
 }
