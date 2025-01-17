4839a86bf94c9fde628b8d3df63bf652b1f234f4
Anshoo Arora <anshooarora@gmail.com>
2025-01-17 13:46:30 -0500

    fixes #42 EmailGenerator: Fails to save file in a location other than target/chaintest

--- core/chaintest-core-java/src/main/java/com/aventstack/chaintest/generator/ChainTestEmailGenerator.java
+++ core/chaintest-core-java/src/main/java/com/aventstack/chaintest/generator/ChainTestEmailGenerator.java
@@ -5,9 +5,9 @@ import com.aventstack.chaintest.domain.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Queue;
@@ -16,14 +16,18 @@ public class ChainTestEmailGenerator extends FileGenerator implements Generator 
 
     private static final Logger log = LoggerFactory.getLogger(ChainTestEmailGenerator.class);
     private static final String NAME = "email";
-    private static final String EMAIL_CLIENT_ENABLED = "chaintest.generator.email.enabled";
+    private static final String BASE_PROPERTY = "chaintest.generator.email";
+    private static final String PROP_ENABLED = BASE_PROPERTY + ".enabled";
+    private static final String PROP_OUT_FILE_NAME = BASE_PROPERTY + ".output-file";
+    private static final String DEFAULT_OUT_DIR = "target/chaintest/";
+    private static final String DEFAULT_OUT_FILE_NAME = "Email.html";
     private static final String TEMPLATE_DIR = "email/";
     private static final String INDEX = "index.ftl";
-    private static final String OUT_FILE = "target/chaintest/Email.html";
 
     private boolean _started;
     private Build _build;
     private String _source;
+    private File _outFile;
 
     public String getSource() {
         return _source;
@@ -37,11 +41,19 @@ public class ChainTestEmailGenerator extends FileGenerator implements Generator 
             return;
         }
 
-        final String enabled = config.get().get(EMAIL_CLIENT_ENABLED);
+        final String enabled = config.get().get(PROP_ENABLED);
         if (!Boolean.parseBoolean(enabled)) {
-            log.debug("{} Generator was not enabled. To enable, set property {}=true in your configuration", NAME, EMAIL_CLIENT_ENABLED);
+            log.debug("{} Generator was not enabled. To enable, set property {}=true in your configuration", NAME, PROP_ENABLED);
             return;
         }
+
+        String outputFileName = Optional.ofNullable(config.get().get(PROP_OUT_FILE_NAME))
+                .filter(name -> !name.isEmpty())
+                .orElse(DEFAULT_OUT_DIR + DEFAULT_OUT_FILE_NAME);
+        if (!(outputFileName.endsWith("htm") || outputFileName.endsWith("html"))) {
+            outputFileName += "/" + DEFAULT_OUT_FILE_NAME;
+        }
+        _outFile = new File(outputFileName);
 
         log.trace("Start was called for testRunner: {}", testRunner);
         _build = build;
@@ -63,7 +75,7 @@ public class ChainTestEmailGenerator extends FileGenerator implements Generator 
             log.debug("No tests to process, skipping flush");
             return;
         }
-        _source = processTemplate(Map.of("build", _build, "tests", tests), OUT_FILE);
+        _source = processTemplate(Map.of("build", _build, "tests", tests), _outFile);
     }
 
     @Override
